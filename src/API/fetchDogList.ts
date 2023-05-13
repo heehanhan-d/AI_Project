5485b28e2eb80453726ce9ba3b850dcba4f9d17b
JEONGHYEON LEE <JEONGHYEONLEE@MacBook-Pro.local>
2023-05-13 17:49:26 +0900

    style: doglist 배열 스타일링 수정

--- src/API/fetchDogList.ts
+++ src/API/fetchDogList.ts
@@ -1,13 +1,25 @@
 import axios from 'axios';
-import { Dog } from '../Component/common/interface';
+import { Dog, DogApiResponse } from '../Component/common/interface';
 
 export const apiUrl = process.env.REACT_APP_API_URL
 
-export const FetchDogList = async () => {
+export const FetchDogList = async (page: number, limit = 20) => {
+
+    const skip = (page - 1) * limit;
+
     try {
-        const result = await axios.get<Dog[]>(`${apiUrl}/underdogs`);
+        const result = await axios.get<DogApiResponse>(`${apiUrl}/underdogs?limit=${limit}&skip=${skip}`);
         return result.data;
     } catch(error) {
         throw Error('데이터 패치에 실패했습니다.')
     }
 }
+
+// export const FetchDogList = async () => {
+//     try {
+//         const result = await axios.get<Dog[]>(`${apiUrl}/underdogs`);
+//         return result.data;
+//     } catch(error) {
+//         throw Error('데이터 패치에 실패했습니다.')
+//     }
+// }
--- src/pages/DogListPage.tsx
+++ src/pages/DogListPage.tsx
@@ -9,67 +9,89 @@ import { DogApiResponse } from '../Component/common/interface';
 
 export const DogListPage = () => {
     const [dogList, setDogList] = useState<Dog[]>([]);
-    
-    console.log(dogList, 'dogList')
+    // console.log(dogList, 'dogList')
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);  
 
     useEffect(() => {
         const fetch = async () => {
             try {
-                const results = await FetchDogList();
-                const message = results.message
-                const data = results.data
-                console.log("message : ", message)
+                const results = await FetchDogList(page);
+                const message = results.message;
+                const data = results.data;
+                setDogList((prev) => [...prev, ...data]);
+                setLoading(false);
+                console.log("message : ", message);
                 console.log("data : ", data);
-                setDogList(data);
+                // setDogList(data);
             } catch(e) {
                 console.error(e);
             }
         }
         fetch();
         // component did mount
-    }, []);
+    }, [page]);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop = document.documentElement.scrollTop;
+            const scrollHeight = document.documentElement.scrollHeight;
+            const clientHeight = document.documentElement.clientHeight;
+
+            if (scrollTop + clientHeight >= scrollHeight - 200 && !loading) {
+                setLoading(true);
+                setPage((prev) => prev + 1);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [loading]);
+    
     return(
         <div>
             <h1>Dog List</h1>
             <ListDiv>
-                <ListCircle></ListCircle>
-                <ListCircle></ListCircle>
-                <ListCircle></ListCircle>
-                <ListCircle></ListCircle>
-            </ListDiv>
-            
-            <DogList dogList = {dogList} />
-            <div>
                 {dogList.map((dog) => (
-                    <div key = {dog.id} >
-                        <img src={dog.img_url} alt={`Dog ${dog.id}`} />
-                        <h2>{dog.breeds?.join(', ')}</h2>
-                        <p>{dog.color}</p>
-                        <p>{String(dog.weight)}</p>
-                        <p>{dog.found.place}</p>
-                        <p>{dog.carecenter?.name}</p>
-                    </div>
+                <ListCircle key = {dog.id}>
+                    <img src={dog.img_url} alt={`Dog ${dog.id}`}/>
+                    <h2>{dog.breeds?.join(', ')}</h2>
+                    <p>{dog.color}</p>
+                    <p>{String(dog.weight)}</p>
+                    <p>{dog.found.place}</p>
+                    <p>{dog.carecenter?.name}</p>
+                </ListCircle>
                 ))}
-            </div>
+            </ListDiv>
+            {loading && <div>Loading...</div>}
         </div>
-    )
-    // <div>{dogList.map((dog) => (dog.breeds))}</div>;
+    );
+    // <div>{dogList.map((dog) => (dog.breeds))}</div>
 };
 
 
 const ListDiv = styled.div`
     display: flex;
+    flex-wrap: wrap;
     width: 100%;
     justify-content: center;
 `
 
 const ListCircle = styled.div`
     display: felx;
-    float: left;
-    width: 20rem;
-    height: 20rem;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 22rem;
+    height: 22rem;
     border-radius: 50%;
     background-color: ${colors.g};
-    margin: 0 1rem;
+    margin: 1rem;
+    flex: 1 0 22%;
+
+    img {
+        max-width: 100%;
+        max-height: 100%;
+    }
 `
