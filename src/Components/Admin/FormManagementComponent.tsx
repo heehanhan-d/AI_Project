51f536e8a0266bb816cc22817bf6d2f7dc23a57d
Hee <heehanhand@gmail.com>
2023-05-27 00:50:50 +0900

    style: 관리자페이지 보호소 예약 목록 조회 스타일링

--- src/Components/Admin/FormManagementComponent.tsx
+++ src/Components/Admin/FormManagementComponent.tsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { fetchFormDataList } from '../../Api/FetchFormDataList';
 import { FormDataType } from '../Common/Interface';
+import styled from 'styled-components';
+import { Colors } from '../Common/Styles';
+import '../Common/Font.css';
 
 export const FormManagement: React.FC = () => {
 
     const [formDataList, setFormDataList] = useState<FormDataType[]>([]);
+    const [selectedFormData, setSelectedFormData] = useState<FormDataType | null>(null);
+
 
     useEffect(() => {
         fetchData();
@@ -25,6 +29,12 @@ export const FormManagement: React.FC = () => {
                 phone: '010-8765-4321',
                 when_day: '2023-06-03',
                 when_time: '18:00'
+            },
+            {
+                name: '삼번',
+                phone: '010-4321-8765',
+                when_day: '2023-06-05',
+                when_time: '13:00'
             }
         ];
     };
@@ -32,111 +42,93 @@ export const FormManagement: React.FC = () => {
     const fetchData = async () => {
         try {
             const response = await axios.get('http://kdt-ai6-team07.elicecoding.com:3001/admin/adopts');
-            const formDataList = response.data;
-            setFormDataList(formDataList);
+            const fetchedData = response.data;
+            setFormDataList(fetchedData);
         } catch (e) {
             console.error(e);
-            setFormDataList([]);
+            setFormDataList(getMockFormDataList());
+            // setFormDataList([]);
         }
-        setFormDataList(formDataList);
-        console.log(formDataList);
     };
+
+    const handleFormSelect = (formData: FormDataType) => {
+        setSelectedFormData(formData);
+    }
 
     return (
         <>
             {/* <h1>Welcome, Admin!</h1>
             보호소 방문 신청 폼 전체를 확인할 수 있어야 해. */}
-            <h1>전체 폼 데이터 조회</h1>
-            <ul>
-                {formDataList.length > 0 ? (
-                    formDataList.map((formData, index) => (
-                        <li key={index}>
-                            <p>이름: {formData.name}</p>
-                            <p>전화번호: {formData.phone}</p>
-                            <p>방문 날짜: {formData.when_day}</p>
-                            <p>방문 시간: {formData.when_time}</p>
-                        </li>
-                    ))
-                ) : (
-                <ul>
-                    {getMockFormDataList().map((formData, index) => (
-                        <li key={index}>
-                            <p>이름: {formData.name}</p>
-                            <p>전화번호: {formData.phone}</p>
-                            <p>방문 날짜: {formData.when_day}</p>
-                            <p>방문 시간: {formData.when_time}</p>
-                        </li>        
-                    ))}
-                </ul>
-                )}
-            </ul>
+            <FormDiv>
+                <h1>보호소 방문 예약 조회</h1>
+                <Table>
+                    <thead>
+                        <tr>
+                            <Th>이름</Th>
+                            <Th>전화번호</Th>
+                            <Th>방문 날짜</Th>
+                            <Th>방문 시간</Th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {formDataList.length > 0 ? (
+                            formDataList.map((formData, index) => (
+                                <tr key={index} onClick={() => handleFormSelect(formData)}>
+                                    <Td>{formData.name}</Td>
+                                    <Td>{formData.phone}</Td>
+                                    <Td>{formData.when_day}</Td>
+                                    <Td>{formData.when_time}</Td>
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <NoDataTd>데이터가 없습니다.</NoDataTd>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+            </FormDiv>
         </>
-    )
-}
+    );
+};
 
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import { fetchFormDataList } from '../../Api/FetchFormDataList';
-// import { FormDataType } from '../Common/Interface';
+const FormDiv = styled.div`
+    width: 60%;
+    height: 450px;
+    padding-top: 30px;
+    margin-top: 60px;
+    font-family: 'UI';
+    border: 5px solid ${Colors.sub};
+    border-radius: 50px;
+    align-items: start;
+`
 
-// export const FormManagement: React.FC = () => {
+const Table = styled.table`
+    width: 80%;
+    border-collapse: collapse;
+    margin: 30px auto;
+`
 
-//     const [formDataList, setFormDataList] = useState<FormDataType[]>([]);
+const Th = styled.th`
+    padding: 8px;
+    text-align: center;
+    background-color: ${Colors.sub};
+    border-bottom: 1px solid ${Colors.g};
+    font-family: 'Logo';
+    font-size: 20px;
+`
 
-//     useEffect(() => {
-//         fetchData();
-//     }, []);
+const Td = styled.td`
+    padding: 8px;
+    border-bottom: 1px solid ${Colors.g};
+    font-family: 'Text';
+    font-size: 18px;
+`
 
-//     // Mock 데이터 배열 생성
-//     const getMockFormDataList = (): FormDataType[] => {
-//         return [
-//             {
-//                 name: '일번',
-//                 phone: '010-1234-5678',
-//                 when_day: '2023-05-30',
-//                 when_time: '10:00'
-//             },
-//             {
-//                 name: '이번',
-//                 phone: '010-8765-4321',
-//                 when_day: '2023-06-03',
-//                 when_time: '18:00'
-//             }
-//         ];
-//     };
-    
-//     const fetchData = async () => {
-//         try {
-//             const response = await axios.get('http://kdt-ai6-team07.elicecoding.com:3001/admin/adopts');
-//             const formDataList = response.data;
-//             setFormDataList(formDataList);
-//         } catch (e) {
-//             console.error(e);
-//             setFormDataList([]);
-//         }
-//         setFormDataList(formDataList);
-//         console.log(formDataList);
-//     };
+const NoDataTd = styled.td`
+    padding: 8px;
+    text-align: center;
+    border-bottom: 1px solid ${Colors.g};
+    color: ${Colors.s};
+`
 
-//     return (
-//         <>
-//             {/* <h1>Welcome, Admin!</h1>
-//             보호소 방문 신청 폼 전체를 확인할 수 있어야 해. */}
-//             <h1>전체 폼 데이터 조회</h1>
-//             <ul>
-//                 {formDataList.length > 0 ? (
-//                     formDataList.map((formData, index) => (
-//                         <li key={index}>
-//                             <p>이름: {formData.name}</p>
-//                             <p>전화번호: {formData.phone}</p>
-//                             <p>방문 날짜: {formData.when_day}</p>
-//                             <p>방문 시간: {formData.when_time}</p>
-//                         </li>
-//                     ))
-//                 ) : (
-//                     <li>데이터가 없습니다.</li>
-//                 )}
-//             </ul>
-//         </>
-//     )
-// }
