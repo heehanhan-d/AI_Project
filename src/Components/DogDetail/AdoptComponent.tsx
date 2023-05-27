7deebc202a738cde6e680b2ba874fb4d1f9ab7b9
Hee <heehanhand@gmail.com>
2023-05-28 03:36:38 +0900

    style: 입양 신청시 로그인 모달창 스타일링

--- src/Components/DogDetail/AdoptComponent.tsx
+++ src/Components/DogDetail/AdoptComponent.tsx
@@ -121,6 +121,9 @@ export default function Adopt() {
             {showLoginModal && (
                 <ModalOverlay>
                     <ModalContent>
+                        <ModalText>
+                            로그인 후 언더독을 유입해보세요.
+                        </ModalText>
                         <LoginForm
                             email={email}
                             password={password}
@@ -169,19 +172,26 @@ export default function Adopt() {
 
     const ModalOverlay = styled.div`
         position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.5);
         display: flex;
         align-items: center;
+        width: 100%;
+        height: 65%;
+        background-color: ${Colors.w};
         justify-content: center;
     `;
 
+    const ModalText = styled.div`
+        padding-bottom: 30px;
+        font-family: 'Logo';
+        font-size: 20pt;
+    `;
+
+
     const ModalContent = styled.div`
-        background-color: white;
-        padding: 20px;
+        background-color: ${Colors.footer};
+        padding: 50px;
+        border-radius: 100px;
+        border: 1px solid ${Colors.s};
     `;
 
     const IngButton = styled(Button)`
@@ -271,221 +281,3 @@ const Reservation = styled.div`
     bottom: 10px;
     margin-bottom: 250px;
 `
-
-// import React, { useState, useEffect } from "react";
-// import styled from 'styled-components';
-// import { Colors, Button } from '../../Components/Common/Styles';
-// import AdoptGuideImg from '../../Img/AdoptGuide.png';
-// import CheckListImg from '../../Img/CheckList.png';
-// import ReservationForm from './ReservationFormComponent';
-// import { DogProfile } from "../../Components/DogDetail/DogProfile";
-// import { CenterRef } from "../Common/Ref";
-
-// export default function Adopt() {
-//     const [shownAdoptInfo, setShownAdoptInfo] = useState(false);
-//     const [shownCheckList, setShownCheckList] = useState(false);
-//     const [shownVisitCenter, setShownVisitCenter] = useState(false);
-
-//     const handleAdoptButtonClick = () => {
-//         setShownAdoptInfo(true);
-//     };
-
-//     const renderAdoptButton = () => {
-//         if (!shownAdoptInfo) {
-//             return (
-//                 <AdoptButton onClick={handleAdoptButtonClick}>
-//                     입양 신청하기
-//                 </AdoptButton>
-//             );
-//         }
-//         return (
-//             <IngButton>입양 신청중</IngButton>
-//         )
-//     };
-
-//     const renderAdoptInfo = () => {
-//         if (shownAdoptInfo) {
-//             return (
-//                 <AdoptGuide>
-//                     <img src={AdoptGuideImg} alt="동물을 입양한 당신이 자랑스럽습니다." />
-//                 </AdoptGuide>
-//             );
-//         }
-//         return null;
-//     };
-
-//     const handleCheckButtonClick = () => {
-//         setShownCheckList(true);
-//     };
-    
-//     const renderCheckButton = () => {
-//         if (!shownCheckList) {
-//             return (
-//                 <CheckButton onClick={handleCheckButtonClick}>
-//                     입양 전 체크리스트 확인하기
-//                 </CheckButton>
-//             );
-//         }
-//         return null;
-//     };
-
-//     const renderCheckList = () => {
-//         if (shownAdoptInfo) {
-//             return (
-//                 <CheckList>
-//                     <img src={CheckListImg} alt="입양 전 주의사항을 확인해보세요." />
-//                 </CheckList>
-//             );
-//         }
-//         return null;
-//     }
-
-//     const handleVisitCenterButtonClick = () => {
-//         setShownVisitCenter(true);
-//     };
-
-//     const renderVisitCenterButton = () => {
-//         if (!shownVisitCenter) {
-//             return (
-//                 <VisitButton onClick={handleVisitCenterButtonClick}>
-//                     Underdog 만나러 가기!
-//                 </VisitButton>
-//             );
-//         }
-//         // return null;
-//     };
-
-//     const renderReservation = () => {
-//         return (
-//             <Reservation>
-//                 <ReservationForm />
-//             </Reservation>
-//         )
-//     }
-
-//     return (
-//         <>
-//                 <DogProfile />
-//                 {renderAdoptButton()}
-//             {shownAdoptInfo &&
-//                 <CenterRef>
-//                     {renderAdoptInfo()}
-//                     {renderCheckButton()}
-//                 </CenterRef>}
-//             {shownCheckList &&
-//                 <CenterRef>
-//                     {renderCheckList()}
-//                     {renderVisitCenterButton()}
-//                 </CenterRef>}
-//             {shownVisitCenter &&
-//                 <CenterRef>
-//                     {renderReservation()}
-//                 </CenterRef>}
-//         </>
-//     )
-// }
-
-//     const AdoptButton = styled(Button)`
-//         justify-content: center;
-//         align-items: center;
-//         position: relative;
-//         padding: 10px 50px;
-//         border: 1px solid ${Colors.main};
-//         border-radius: 300px;
-//         background-color: ${Colors.main};
-//         color: ${Colors.w};
-//         font-family: "Logo";
-//         font-size: 20px;
-//         top: -70px;
-//         left: -425px;
-//         cursor: pointer;
-//         z-index: 10px;
-//     `;
-
-//     const IngButton = styled(Button)`
-//         justify-content: center;
-//         align-items: center;
-//         position: relative;
-//         padding: 10px 50px;
-//         border: 3px dashed ${Colors.main};
-//         border-radius: 300px;
-//         background-color: ${Colors.w};
-//         color: ${Colors.b};
-//         font-family: "Logo";
-//         font-size: 20px;
-//         top: -70px;
-//         left: -425px;
-//         cursor: pointer;
-//         z-index: 10px;
-//         `;
-    
-//     const AdoptGuide = styled.div`
-//         display: flex;
-//         justify-content: center;
-//         align-item: center;
-//         position: relative;
-//         width: 80%;
-//         height: 600px;
-//         top: -60px;
-//         margin: 150px auto 180px auto;
-//     `
-
-//     const CheckButton = styled(Button)`
-//         justify-content: center;
-//         align-items: center;
-//         position: relative;
-//         padding: 10px 50px;
-//         border: 1px solid ${Colors.footer};
-//         border-radius: 300px;
-//         background-color: ${Colors.footer};
-//         color: ${Colors.w};
-//         font-family: "Logo";
-//         font-size: 20px;
-//         top: -250px;
-//         margin: 0 auto;
-//         bottom: 325px;
-//         cursor: pointer;
-//     `;
-
-// const CheckList = styled.div`
-//         display: flex;
-//         justify-content: center;
-//         align-item: center;
-//         position: relative;
-//         width: 80%;
-//         height: 600px;
-//         margin: 0 auto 200px auto;
-//         top: -80px;
-//     `   
-
-// const VisitButton = styled(Button)`
-//     justify-content: center;
-//     align-items: center;
-//     position: absolute;
-//     padding: 10px 50px;
-//     border: 1px solid ${Colors.main};
-//     border-radius: 300px;
-//     background-color: ${Colors.main};
-//     color: ${Colors.w};
-//     font-family: "Logo";
-//     font-size: 20px;
-//     left: 40%;
-//     top: 2100px;
-//     cursor: pointer;
-//     z-index: 10px;
-// `;
-
-// const Reservation = styled.div`
-//     position: relative;
-//     justify-content: center;
-//     align-items: center;
-//     width: 90%;
-//     height: 500px;
-//     color: ${Colors.b};
-//     background-color: ${Colors.w};
-//     border: 10px solid ${Colors.sub};
-//     border-radius: 30px;
-//     padding: 30px;
-//     bottom: 10px;
-//     margin-bottom: 250px;
-// `
--- src/Components/DogDetail/LoginFormComponent.tsx
+++ src/Components/DogDetail/LoginFormComponent.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import styled from 'styled-components';
+import '../Common/Font.css';
+import { Button, Colors } from '../Common/Styles';
 
 export const LoginForm = ({ email, password, setEmail, setPassword, onLogin, onClose }: any) => {
   const handleEmailChange = (e: any) => {
@@ -18,13 +21,79 @@ export const LoginForm = ({ email, password, setEmail, setPassword, onLogin, onC
     onClose();
   };
 
-  return (
-    <form>
-      <input type="email" value={email} onChange={handleEmailChange} placeholder="이메일" />
-      <input type="password" value={password} onChange={handlePasswordChange} placeholder="비밀번호" />
-      <button type="submit" onClick={handleLogin}>로그인</button>
-      <button onClick={handleClose}>닫기</button>
-    </form>
+    return (
+        <>
+            <Form>
+                <InputContainer>
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={handleEmailChange} placeholder="이메일"
+                            style={{
+                            padding: '3px',
+                            border: 'none',
+                            outline: 'none',
+                            fontFamily: 'Text',
+                            fontSize: '15pt',
+                            }}
+                        />
+                </InputContainer>
+                <InputContainer>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={handlePasswordChange} placeholder="비밀번호"
+                            style={{
+                            padding: '3px',
+                            border: 'none',
+                            outline: 'none',
+                            fontFamily: 'Text',
+                            fontSize: '15pt',
+                            }}
+                        />
+                </InputContainer>
+            </Form>
+            <ButtonDiv>
+                <SubmitButton type="submit" onClick={handleLogin}>로그인</SubmitButton>
+                <CloseButton onClick={handleClose}>닫기</CloseButton>
+            </ButtonDiv>
+    </>
   );
 };
 
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    margin: 0 auto;
+`
+
+const InputContainer = styled.div`
+    padding: 1px;
+    background-color: ${Colors.w};
+    border: 2px solid ${Colors.g};
+    border-radius: 30px;
+    margin: 5px;
+`
+
+const ButtonDiv = styled.div`
+    display: flex;
+`
+
+const SubmitButton = styled(Button)`
+    margin: 20px 5px 0 70px;
+    padding: 10px 35px;
+    border: 2px solid ${Colors.main};
+    border-radius: 30px;
+    background-color: ${Colors.main};
+    font-family: 'Logo';
+`
+
+const CloseButton = styled(Button)`
+    margin: 20px 50px 0 10px;
+    padding: 10px 40px;
+    border: 2px solid ${Colors.s};
+    border-radius: 30px;
+    background-color: ${Colors.s};
+    font-family: 'Logo';
+`
